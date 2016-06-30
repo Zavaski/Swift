051bf96a966c72defb998c37791e8d9afaeeabfc
Gregory Mendes <gremendes@hotmail.com>
2016-06-30 09:26:24 -0300

    adv.

---
+++ adv.swift
@@ -0,0 +1,20 @@
+//
+//  adv.swift
+//  bjj
+//
+//  Created by Student on 6/30/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+import Foundation
+class adv{
+    var categoria: String!
+    var movimento: String!
+    var img: String!
+    
+    init(categoria: String, movimento: String, img: String)
+    {
+        self.categoria = categoria
+        self.movimento = movimento
+        self.img = img
+    }
+}
